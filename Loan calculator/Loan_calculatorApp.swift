de33ded54ac56355d919495f8df5e9bf1ac7ea76
lukepatterson18 <lpatterson@scu.edu>
2022-02-12 11:03:15 -0800

    Initial Commit

---
+++ Loan calculator/Loan_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loan_calculatorApp.swift
+//  Loan calculator
+//
+//  Created by Luke Patterson on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Loan_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
